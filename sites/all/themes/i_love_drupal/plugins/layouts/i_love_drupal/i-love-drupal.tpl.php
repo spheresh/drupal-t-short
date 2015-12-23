9261418e8572192e0d818c7a674793e9201cac51
Loparev <pavel.loparev@propeople.com.ua>
2015-12-24 00:35:39 +0200

    Load more

--- sites/all/themes/i_love_drupal/plugins/layouts/i_love_drupal/i-love-drupal.tpl.php
+++ sites/all/themes/i_love_drupal/plugins/layouts/i_love_drupal/i-love-drupal.tpl.php
@@ -25,24 +25,28 @@
 
 <?php if ($content['region-wtf']): ?>
   <section id="wtf">
+    <h2>Что это?</h2>
     <?php print $content['region-wtf']; ?>
   </section>
 <?php endif; ?>
 
 <?php if ($content['region-t-shorts']): ?>
   <section id="t-shorts">
+    <h2>Футболки</h2>
     <?php print $content['region-t-shorts']; ?>
   </section>
 <?php endif; ?>
 
 <?php if ($content['region-team']): ?>
   <section id="team">
+    <h2>Команда</h2>
     <?php print $content['region-team']; ?>
   </section>
 <?php endif; ?>
 
 <?php if ($content['region-contacts']): ?>
   <section id="contacts">
+    <h2>Контакты</h2>
     <?php print $content['region-contacts']; ?>
   </section>
 <?php endif; ?>
